13b48d0baef3f6c79a4e1df94f7624fb4f0b77ab
Gereon Dusella <git@gereondusella.de>
2022-11-22 14:31:25 +0100

    added print flush to preprocess

--- hub/deployment/files/sedona/preprocess.sh
+++ hub/deployment/files/sedona/preprocess.sh
@@ -3,7 +3,7 @@
 echo "Preprocessing data"
 docker pull registry.gitlab.com/zergar/benchi/preprocess:0.2.3-13
 echo "benchi_marker,$(date +%s.%N),start,preprocess,sedona,,"
-docker run -v $(dirname $0)/../../data:/data --name "preprocess_sedona" --rm registry.gitlab.com/zergar/benchi/preprocess:0.2.3-13 python preprocess.py $1
+docker run -e PYTHONUNBUFFERED=1 -v $(dirname $0)/../../data:/data --name "preprocess_sedona" --rm registry.gitlab.com/zergar/benchi/preprocess:0.2.3-13 python preprocess.py $1
 echo "benchi_marker,$(date +%s.%N),end,preprocess,sedona,,"
 
 echo "Starting Container in background"
